522b316f21df42e4ae8124c9571d844419f99ecd
mitul72 <mituldhawan2@gmail.com>
2024-04-16 08:37:50 -0600

    update router

--- routes/api.ts
+++ routes/api.ts
@@ -5,13 +5,13 @@ const router = Router();
 
 const products: Object[] = [];
 
-router.post("/add_product", (req, res) => {
+router.post("/add_product", async (req, res) => {
     console.log("Connect to server successfully");
     console.log(req.body);
     const pdata = req.body;
     let isTrue = false;
     console.log(pdata.name);
-    prisma.inventory.create({
+    const newInventory = await prisma.inventory.create({
       data: {
         itemName: pdata.name,
         itemSize: pdata.size,
@@ -24,20 +24,122 @@ router.post("/add_product", (req, res) => {
         type: pdata.type,
       },
     });
+
     products.push(pdata);
     res.status(200).json({
       status_code: 200,
       message: "Product created",
-      inventory: pdata,
+      inventory: newInventory,
     });
   });
   
   router.get("/get_products", (req, res) => {
-    if (products.length > 0) {
+    prisma.$connect();
+    prisma.inventory.findMany().then((data) => {
+      console.log(data);
+      if (data.length > 0) {
       res.status(200).json({
         status_code: 200,
         message: "Product list",
-        inventory: products,
+        inventory: data,
+      });
+    } else {
+      res.status(200).json({
+        status_code: 200,
+        message: "No products found",
+        inventory: [],
+      });
+    }
+    }
+    );
+  }
+  );
+  router.delete("/delete_product/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const inventory = await prisma.inventory.delete({
+      where: {
+        itemID: id,
+      },
+    });
+    if(inventory){
+      res.status(200).json({
+        status_code: 200,
+        message: "Product deleted",
+        inventory: inventory,
+      });
+    } else {
+      res.status(200).json({
+        status_code: 200,
+        message: "Product not found",
+        inventory: [],
+      });
+    }
+  });
+
+  router.put("/update_product/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const pdata = req.body;
+    console.log(id);
+    const inventory = await prisma.inventory.update({
+      where: {
+        itemID: id,
+      },
+      data: {
+        itemName: pdata.name,
+        itemSize: pdata.size,
+        itemDescription: pdata.description,
+        checkInDate: pdata.checkInDate,
+        location: pdata.location,
+        itemStatus: pdata.status,
+        itemQuantity: parseInt(pdata.quantity),
+        type: pdata.type,
+      },
+    });
+    if(inventory){
+      res.status(200).json({
+        status_code: 200,
+        message: "Product updated",
+        inventory: inventory,
+      });
+    } else {
+      res.status(200).json({
+        status_code: 200,
+        message: "Product not found",
+        inventory: [],
+      });
+    }
+  });
+
+  router.get("/get_product/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const inventory = await prisma.inventory.findUnique({
+      where: {
+        itemID: id,
+      },
+    });
+    if(inventory){
+      res.status(200).json({
+        status_code: 200,
+        message: "Product found",
+        inventory: inventory,
+      });
+    } else {
+      res.status(200).json({
+        status_code: 200,
+        message: "Product not found",
+        inventory: [],
+      });
+    }
+  });
+  router.delete("/delete_all", async (req, res) => {
+    const inventory = await prisma.inventory.deleteMany();
+    if(inventory){
+      res.status(200).json({
+        status_code: 200,
+        message: "All products deleted",
+        inventory: inventory,
       });
     } else {
       res.status(200).json({
@@ -47,4 +149,37 @@ router.post("/add_product", (req, res) => {
       });
     }
   });
+
+  // router.post("/test_add", async (req, res) => {
+  //   // prisma.$connect();
+  //   console.log("Connect to server successfully");
+  //   // console.log(req.body);
+  //   // const pdata = req.body;
+  //   let isTrue = false;
+  //   // console.log(pdata.name);
+  //   try{
+  //   const newInventory = await prisma.inventory.create({
+  //     data: {
+  //       itemName: "pdata.name",
+  //       itemSize: "pdata.size",
+  //       itemDescription: "pdata.description",
+  //       checkInDate: "pdata.checkInDate",
+  //       location: "pdata.location",
+  //       itemStatus:true,
+  //       itemQuantity: 100,
+  //       itemID:102,
+  //       type: "pdata.type",
+  //     },
+  //   });
+  //   res.status(200).json({
+  //     status_code: 200,
+  //     message: "Product created",
+  //     inventory: newInventory,
+  //   });
+
+  // }catch(err){
+  //   console.log(err);
+  // }
+  //   // products.push(pdata);
+  // });
 export default router;
