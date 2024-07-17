137cad3f248fe3f1b6acf52864df9c8784d5e0fc
mitul72 <mituldhawan2@gmail.com>
2024-07-17 16:38:45 -0600

    update backend to fix fetch profile

--- routes/user.ts
+++ routes/user.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import prisma from "../data/prisma.js";
+import { expressjwt } from "express-jwt";
 
 const router = Router();
+const key = process.env.SECRET_KEY;
+if (!key) {
+    throw new Error("SECRET_KEY environment variable is not defined");
+}
+const jwtMiddleware = expressjwt({ secret: key, algorithms: ['HS256'] });
 
-router.get("/profile/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
+router.get("/get_user",jwtMiddleware, async (req, res) => {
+    const id = (req as any).auth.id;
+    console.log(id);
     const users = await prisma.user.findUnique(
         {
             where: {
