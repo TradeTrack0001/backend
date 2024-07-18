a16019a308fe9c23d65aabac2e3a5a45861c6c33
Ruvalpreet <123599263+Ruvalpreet@users.noreply.github.com>
2024-07-17 19:57:46 -0600

    profile changes

--- routes/user.ts
+++ routes/user.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import prisma from "../data/prisma.js";
+import { expressjwt } from "express-jwt";
 
 const router = Router();
+const SECRET_KEY = process.env.SECRET_KEY;
 
-router.get("/profile/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
+if (!SECRET_KEY) {
+    throw new Error('SECRET_KEY environment variable is not defined');
+  }
+
+const jwtMiddleware = expressjwt({ secret: SECRET_KEY, algorithms: ['HS256'] });
+
+router.get("/get_user", jwtMiddleware,async (req, res) => {
+    const id = (req as any).auth.id;
+    console.log("This is the id: ", id);
     const users = await prisma.user.findUnique(
         {
             where: {
@@ -14,6 +23,7 @@ router.get("/profile/:id", async (req, res) => {
     );
     res.json(users);
 })
+
 
 
 export default router
